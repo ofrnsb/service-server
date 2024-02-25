1729c596447e2faaf396e6f8a45c59c3ff964bf0
ofrnsb <ondifransbutarbutar@upi.edu>
2024-02-25 20:49:46 +0700

    init

---
+++ src/test/java/com/ob/serviceserver/ServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ob.serviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
